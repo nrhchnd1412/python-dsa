97f042f8cb4669f94b2abf886c51867e012af005
Neeraj Chand <neerajchand@Neerajs-MacBook-Air.local>
2025-06-27 11:54:51 +0530

    backtracking

---
+++ backtracking/permutations.py
@@ -0,0 +1,56 @@
+'''
+📌 Problem Statement:
+
+Given an array nums of distinct integers, return all the possible permutations of the array.
+
+'''
+
+def permutations(nums):
+    # Time Complexity: O(n!) — Total permutations of n distinct elements.
+    # Space Complexity: O(n)
+    def backtrack(path,used):
+        if len(path)==len(nums):
+            results.append(path[:])
+            return
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+            used[i]=True
+            path.append(nums[i])
+            backtrack(path,used)
+
+            used[i]=False
+            path.pop()
+    results=[]
+    used=[False]* len(nums)
+    backtrack([],used)
+    return results
+
+def permutations_with_duplicates(nums):
+    '''
+    Time Complexity: O(n! * n) in the worst case.
+    Space Complexity: O(n) for recursion and used array.
+    '''
+    def backtrack(path,used):
+        if len(path)==len(nums):
+            results.append(path[:])
+            return
+        for i in range(len(nums)):
+            if used[i]:
+                continue
+            if i >0 and nums[i]==nums[i-1] and not used[i-1]:
+                continue
+            used[i]=True
+            path.append(nums[i])
+            backtrack(path,used)
+
+            used[i]=False
+            path.pop()
+    results=[]
+    nums.sort()
+    used=[False]* len(nums)
+    backtrack([],used)
+    return results
+
+print(permutations([1,1,2]))
+print(permutations_with_duplicates([1,1,2]))
---
+++ backtracking/power_sets_duplicates.py
@@ -0,0 +1,27 @@
+'''
+📌 Problem Statement:
+
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets, and the elements in a subset can be in any order.
+
+Time Complexity: O(2ⁿ × n)
+Space Complexity: O(2ⁿ) for storing all subsets
+'''
+
+def power_set(nums):
+    def backtrack(start,path):
+        results.append(path[:])
+        for i in range(start,len(nums)):
+            if i>start and nums[i]==nums[i-1]:
+                continue
+            path.append(nums[i])
+            backtrack(i+1,path)
+            path.pop()
+    results=[]
+    nums.sort()
+    backtrack(0,[])
+    return results
+
+nums=[1,2,2,3,3]
+print(power_set(nums))
