01fa1487b53b51faa3399bb4124b3a88a26c799b
Chand <neeraj.chand@kochcc.com>
2025-07-04 14:28:45 +0530

    restructure

--- patterns/sliding_window/max_sum_subarray.py
+++ patterns/sliding_window/max_sum_subarray.py
@@ -1,19 +1,11 @@
 '''
-Naive: O(n*k)
-'''
-
-def max_sum_subarray_naive(arr,k):
-    max_sum=float('-inf')
-    for i in range(len(arr)-k+1):
-        current_sum= sum(arr[i:i+k])
-        max_sum=max(max_sum,current_sum)
-    return max_sum
-
-'''
-sliding window: O(n)
+Problem: Find the maximum sum of any subarray of fixed size k.
+works with -ve numbers as well.
 '''
 
 def max_sum_subarray_sliding_window(arr,k):
+    # Time: O(n)
+    # Space: O(1)
     window_sum=sum(arr[:k])
     max_sum=window_sum
     for i in range(k,len(arr)):
@@ -21,5 +13,4 @@ def max_sum_subarray_sliding_window(arr,k):
         max_sum=max(max_sum,window_sum)
     return max_sum
 
-print(max_sum_subarray_naive([1,2,10,4,5,6],3))
 print(max_sum_subarray_sliding_window([1,2,10,4,5,6],3))
